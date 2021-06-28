7c5ea1ee089907ed74151af3fdebf3a6b3059a55
BoomanWill <76132689+BoomanWill@users.noreply.github.com>
2021-06-28 09:57:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,5 @@
 sudo apt-get install python3-pip
 sudo apt-get install wmctrl
 sudo pip install keyboard
-sudo mv sensors.sh ~
-sudo mv sensors.py ~
-cd --
-sudo chmod +x sensors.sh
+sudo mv pythonsensors.py ~
+
