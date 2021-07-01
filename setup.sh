2606a07d1bf0fefbe6be5806a8a4044d7e369ee3
BoomanWill <76132689+BoomanWill@users.noreply.github.com>
2021-07-01 12:14:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,8 @@ sudo apt-get install python3-pip
 sudo apt-get install wmctrl
 sudo pip install keyboard
 sudo pip install psutil
+pip install psutil
+sudo pip install matplotlib
+pip install matplotlib
 sudo mv hwmonitor.py ~
 
