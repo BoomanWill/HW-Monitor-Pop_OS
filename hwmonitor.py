e226b965de6901a9bc5252c3ba84f77ca7664a6b
BoomanWill <76132689+BoomanWill@users.noreply.github.com>
2021-07-01 11:21:41 +0100

    Update hwmonitor.py

--- hwmonitor.py
+++ hwmonitor.py
@@ -2,6 +2,8 @@ import os
 import time
 import subprocess
 import keyboard
+import matplotlib.pyplot as plt
+import psutil
 
 def getinfo():
     cpuhigh = 0
@@ -15,18 +17,46 @@ def getinfo():
     auxtin2high = 0
     smbusmasterhigh = 0
     gpumemhigh = 0
+    memhigh = 0
+    cpuusehigh = 0
+    cpufreqhigh = 0
+    cpugraph = []
+    gpugraph = []
+    nvmegraph = []
+    smbusgraph = []
+    timelist = []
+    timenum = 0 
 
     while True:
         if not keyboard.is_pressed('q'):
+            timenum += 0.01
+            timelist.append(timenum)
+
+            cpufreq = psutil.cpu_freq()[0]
+            cpufreq = round(cpufreq / 1000, 1)
+            if cpufreq > cpufreqhigh:
+                cpufreqhigh = cpufreq
+
+            cpuusage = psutil.cpu_percent(0.1)
+            if cpuusage > cpuusehigh:
+                cpuusehigh = cpuusage
+
+            memusage = psutil.virtual_memory()[3]
+            memusage = round(memusage / 1000000000, 2)
+            if memusage > memhigh:
+                memhigh = memusage
+
             cputemp = subprocess.getoutput('sensors | grep Tccd1')
             cputemp = float(cputemp[-8:-4])
             if cputemp > cpuhigh:
                 cpuhigh = cputemp
+            cpugraph.append(cputemp)
 
             gputemp = subprocess.getoutput('nvidia-smi | grep %')
             gputemp = float(gputemp[8:10])
             if gputemp > gpuhigh:
                 gpuhigh = gputemp
+            gpugraph.append(gputemp)
 
             gpumem = subprocess.getoutput('nvidia-smi | grep %')
             gpumem = float(gpumem[36:40])
@@ -37,6 +67,7 @@ def getinfo():
             nvmetemp = float(nvmetemp[15:19])
             if nvmetemp > nvmehigh:
                 nvmehigh = nvmetemp
+            nvmegraph.append(nvmetemp)
 
             vcore = subprocess.getoutput('sensors | grep in0')
             vcore = float(vcore[25:29])
@@ -72,25 +103,56 @@ def getinfo():
             smbusmaster = float(smbusmaster[25:29])
             if smbusmaster > smbusmasterhigh:
                 smbusmasterhigh = smbusmaster
+            smbusgraph.append(smbusmaster)
         
             os.system('clear')
-            print('GPU temp:    {}C HIGH:   {}C'.format(gputemp, gpuhigh))
-            print('GPU mem:     {}MiB High: {}MiB'.format(gpumem, gpumemhigh))
-            print('CPU temp:    {}C HIGH:   {}C'.format(cputemp, cpuhigh))
-            print('NVME temp:   {}C HIGH:   {}C'.format(nvmetemp, nvmehigh))
-            print('Vcore:       {}V HIGH:   {}V'.format(vcore, vcorehigh))
-            print('Vbat:        {}V HIGH:   {}V'.format(vbat, vbathigh))
-            print('SYSTIN:      {}C HIGH:   {}C'.format(systin, systinhigh))
-            print('CPUTIN:      {}C HIGH:   {}C'.format(cputin, cputinhigh))
-            print('AUXTIN0:     {}C HIGH:   {}C'.format(auxtin0, auxtin0high))
-            print('AUXTIN2:     {}C HIGH:   {}C'.format(auxtin2,auxtin2high))
-            print('SMBUSMASTER: {}C HIGH:   {}C'.format(smbusmaster, smbusmasterhigh))
+            print('----------------------------------')
+            print('GPU temp:    {}C | HIGH:   {}C'.format(gputemp, gpuhigh))
+            print('----------------------------------')
+            print('GPU mem:     {}MiB | High: {}MiB'.format(gpumem, gpumemhigh))
+            print('----------------------------------')
+            print('CPU temp:    {}C | HIGH:   {}C'.format(cputemp, cpuhigh))
+            print('----------------------------------')
+            print('NVME temp:   {}C | HIGH:   {}C'.format(nvmetemp, nvmehigh))
+            print('----------------------------------')
+            print('Vcore:       {}V | HIGH:   {}V'.format(vcore, vcorehigh))
+            print('----------------------------------')
+            print('Vbat:        {}V | HIGH:   {}V'.format(vbat, vbathigh))
+            print('----------------------------------')
+            print('SYSTIN:      {}C | HIGH:   {}C'.format(systin, systinhigh))
+            print('----------------------------------')
+            print('CPUTIN:      {}C | HIGH:   {}C'.format(cputin, cputinhigh))
+            print('----------------------------------')
+            print('AUXTIN0:     {}C | HIGH:   {}C'.format(auxtin0, auxtin0high))
+            print('----------------------------------')
+            print('AUXTIN2:     {}C | HIGH:   {}C'.format(auxtin2,auxtin2high))
+            print('----------------------------------')
+            print('SMBUSMASTER: {}C | HIGH:   {}C'.format(smbusmaster, smbusmasterhigh))
+            print('----------------------------------')
+            print('CPU usage:   {}% | HIGH:   {}%'.format(cpuusage, cpuhigh))
+            print('----------------------------------')
+            print('Mem used     {}GB | High:  {}GB'.format(memusage, memhigh))
+            print('----------------------------------')
+            print('CPU freq     {}Ghz | High: {}GHz'.format(cpufreq, cpufreqhigh))
+            print('----------------------------------')
             print('Press Q to exit')
-            time.sleep(0.5)
+            time.sleep(0.01)
         else:
+            print('Generating graph...')
+            time.sleep(1)
+            plt.plot(timelist, cpugraph, label='CPU')
+            plt.plot(timelist, gpugraph, label='GPU')
+            plt.plot(timelist, nvmegraph, label='NVME')
+            plt.plot(timelist, smbusgraph, label='SMBUSMASTER')
+            plt.ylabel('Temp(C)')
+            plt.axis([0, timelist[-1], 0, 100])
+            plt.legend()
+            plt.show()
             os.system('clear')
             exit()
         
 
 
 getinfo()
+
+
